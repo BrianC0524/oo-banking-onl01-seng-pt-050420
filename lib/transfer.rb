c31e4f6d4df8a928efc5ca0ec31edf769c2700f6
Brian Craycraft <craycraftb24@hotmail.com>
2020-06-16 20:51:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,31 @@
 class Transfer
-  # your code here
+  attr_accessor :amount, :status
+  attr_reader :sender, :receiver
+  
+  def initialize(sender, receiver, amount = 50, status = 'pending')
+    
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+    
+  end
+  
+  def valid?
+    
+    sender.valid?
+    receiver.valid?
+    
+  end
+  
+  def execute_transaction
+    count = 0
+    while count < 1 do
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = 'complete'
+      count++
+    
+  end
+  
 end
